3b0a73863927b719ca946f3acb3463f79c241356
indirvel <indira.velupuri@gmail.com>
2020-07-01 04:15:49 +0530

    new project for jenkins

---
+++ HelloWorld/src/main/java/com/dhl/example/App.java
@@ -0,0 +1,22 @@
+package com.dhl.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App{ 
+
+	public static void main( String[] args ){
+        App app = new App();
+        int addition = app.sum(10, 20);
+    	System.out.println( "Addition : "+addition);
+    }
+    
+    
+    public int sum(int a, int b){
+    	return a+b;
+    }
+    
+    
+}
+
